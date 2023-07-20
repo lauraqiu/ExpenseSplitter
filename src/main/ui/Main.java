cde6fffab71163bc2cd22aaadc71c5347e01fce4
lauraqiu <lauraqiu21@gmail.com>
2023-07-19 18:43:57 -0700

    fixed bug in main

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -8,26 +8,35 @@ import java.util.Scanner;
 public class Main {
     static Scanner console = new Scanner(System.in);
 
+    @SuppressWarnings("methodlength")
     public static void main(String[] args) {
         ExpenseList expenseList = new ExpenseList();
         System.out.println("Hello, what would you like to do?");
-        System.out.println("Enter a to add an expense to split, d to delete an split expense, "
-                + "or v to view your split expense tracker");
-        String choice = console.nextLine();
+        boolean quit = false;
 
-        if (choice.equalsIgnoreCase("a")) {
-            Expense expense = splitExpense();
-            expenseList.addExpense(expense);
-            System.out.println("Your expense has been split and added to the expense tracker");
-        } else if (choice.equalsIgnoreCase("d")) {
-            System.out.println("Please enter the name of the expense you would like to delete");
-            String name = console.nextLine();
-            expenseList.deleteExpense(name);
-        } else if (choice.equalsIgnoreCase("v")) {
-            for (int i = 0; i < expenseList.getSize(); i++) {
-                System.out.println("Your expense " + expenseList.getExpense(i).getName() + "had a total of "
-                        + expenseList.getExpense(i).getInitExpense() + ". Each person owes $"
-                        + expenseList.getExpense(i).getSplitExpense());
+        while (!quit) {
+            System.out.println("Enter a to add an expense to split, d to delete an split expense, "
+                    + "or v to view your split expense tracker or q to quit: ");
+            String choice = console.nextLine();
+
+            if (choice.equalsIgnoreCase("a")) {
+                Expense expense = splitExpense();
+                expenseList.addExpense(expense);
+                System.out.println("Your expense has been split and added to the expense tracker");
+            } else if (choice.equalsIgnoreCase("d")) {
+                System.out.println("Please enter the name of the expense you would like to delete");
+                String name = console.nextLine();
+                expenseList.deleteExpense(name);
+                System.out.println("The expense " + name + "has been deleted");
+            } else if (choice.equalsIgnoreCase("v")) {
+                for (int i = 0; i < expenseList.getSize(); i++) {
+                    System.out.println("Your expense " + expenseList.getExpense(i).getName() + " had a total of $"
+                            + expenseList.getExpense(i).getInitExpense() + ". Each person owes $"
+                            + expenseList.getExpense(i).getSplitExpense());
+                }
+            } else if (choice.equalsIgnoreCase("q")) {
+                System.out.println("Exiting program");
+                quit = true;
             }
         }
     }
@@ -38,16 +47,17 @@ public class Main {
 
         System.out.println("What was the total cost?");
         int cost = console.nextInt();
+        console.nextLine();
 
         System.out.println("How many people do you want to split it between?");
         int numPeople = console.nextInt();
+        console.nextLine();
 
         int splitCost = cost / numPeople;
-        System.out.println("For the expense: " + name);
-        System.out.println("Each person owes: " + splitCost);
+        System.out.println("Expense name: " + name);
+        System.out.println("Each person owes: $" + splitCost);
 
-        Expense expense = new Expense(cost, splitCost, name);
-        return expense;
+        return new Expense(cost, splitCost, name);
     }
 }
 
